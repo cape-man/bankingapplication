d45e97823f7c358a29f5a74af566af076451fd2f
Shiny Brito <M1055945@mindtree.com>
2020-05-24 22:15:42 +0530

    host info added

--- server/app.js
+++ server/app.js
@@ -1,32 +1,34 @@
-
 /**
  * Module dependencies.
  */
-var express   = require('express'),
-    http      = require('http'),
-    path      = require('path'),
-    bodyParser = require('body-parser'),
-    methodOverride = require('method-override');
+var express = require('express'),
+  http = require('http'),
+  path = require('path'),
+  bodyParser = require('body-parser'),
+  methodOverride = require('method-override');
 
 var app = express();
-exports.startServer = function() {
-app.set('port', process.env.PORT || 3300);
-app.engine('.html', require('ejs').__express);
-app.set('views', path.join(__dirname, '../public/views'));
-app.set('view engine', 'html');
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, '../public')));
+exports.startServer = function () {
+  app.set('port', process.env.PORT || 3300);
+  app.set('host', process.env.HOST );
+  app.engine('.html', require('ejs').__express);
+  app.set('views', path.join(__dirname, '../public/views'));
+  app.set('view engine', 'html');
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  app.use(methodOverride('_method'));
+  app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('/bank', function(req,res){
-  res.render('index', {
+  app.get('/bank', function (req, res) {
+    res.render('index', {
       // key : value
     });
-});
-app.get('/*');
+  });
+  app.get('/*');
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Demo server listening on port ' + app.get('port'));
-});
+  http.createServer(app).listen(app.get('port'), function () {
+    console.log('Demo server listening on port ' + app.get('port') + app.get('host'));
+  });
 
 };
