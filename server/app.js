8ee6463a784addbaca9fe830e6a1d9e258aaa655
Shiny Brito <M1055945@mindtree.com>
2020-05-25 11:25:15 +0530

    host set

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ var express = require('express'),
 var app = express();
 exports.startServer = function () {
   app.set('port', process.env.PORT || 3300);
-  app.set('host', "https://cape-test.southeastasia.cloudapp.azure.com/" );
+  app.set('host', "https://cape-test.southeastasia.cloudapp.azure.com" );
   app.engine('.html', require('ejs').__express);
   app.set('views', path.join(__dirname, '../public/views'));
   app.set('view engine', 'html');
